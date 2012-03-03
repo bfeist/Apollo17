005dc336a902d1c0b24fccf5005501e7ee8b3568
bfeist <bf@benfeist.com>
2012-03-03 18:08:32 -0500

    First complete MC output for pages 1-500

--- MC_Processing/processpipe.py
+++ MC_Processing/processpipe.py
@@ -1,27 +1,68 @@
 import csv
 import sys 
-    
-pageCounter = 0
+import re
 
-reader = csv.reader(open("test2.csv", "rU"), delimiter='|')
+def sterilize_token(token):
+    bs0 = BadNumberSub(0, ["o","Q","O"])
+    bs1 = BadNumberSub(1, ["i","J", "I","!","L","l"])
+    bs4 = BadNumberSub(4, ["h"])
+    bs8 = BadNumberSub(8, ["B"])
+    
+    tempToken = token
+    
+    for badSub in [ bs0, bs1, bs4, bs8 ]:
+        for sub in badSub.badSubList:
+            tempToken = tempToken.replace(sub, str(badSub.number))
+    
+    return tempToken
+
+def scrub_timestamp(timestamp):
+    values =  re.split(" ", timestamp);
+    i = 0
+    days = 0    
+    days = sterilize_token(values[i])
+    i += 1
+    hours = sterilize_token(values[i])
+    i += 1
+    minutes = sterilize_token(values[i])
+    i += 1
+    seconds = sterilize_token(values[i])
+    
+    cleanTimestamp = days + " " + hours + " " + minutes + " " + seconds
+    return cleanTimestamp
+    
+class BadNumberSub:
+    def __init__(self, number, badSubList):
+        self.number = number
+        self.badSubList = badSubList
+
+def scrub_callsign(callsign):
+    if callsign == "MCC":
+        callsign = "CC"
+    return callsign
+
+pageCounter = 9
+
+reader = csv.reader(open("F:\ApolloGit\Apollo17WIP\OCR Output\A17_TEC_1-500.csv", "rU"), delimiter='|')
 for row in reader:
     #print(row)
-    if row[0].startswith("Tape") or row[5].startswith("Tape"):
+    if row[0].startswith("Tape") or row[2].startswith("Tape"):
     #if new page
         if row[0].startswith("Tape"):
             tapeNumber = row[0]
-        if row[5].startswith("Tape") :
-            tapeNumber = row[5]
+        if row[2].startswith("Tape") :
+            tapeNumber = row[2]
             
         pageCounter += 1
-        print "\n\t{0}\n\tPage {1}".format(tapeNumber, pageCounter)
-    elif row == ['', '', '', '', '', '', '']:
-        pass
-        #print "-----------------------blank row"
-    elif row[4] == '' or row[4] == 'APOLLO':
+        print "\t{0}\n\tPage {1}".format(tapeNumber, pageCounter)
+        #print "\t{0}".format(tapeNumber, pageCounter)
+    elif row[0].startswith("Page") or row[2].startswith("Page"):
+        print "-----------------------" + str(pageCounter)
+    elif row[2] == '' or row[2] == 'APOLLO' or row[1] == 'APOLLO' or row[2] == 'END OF TAPE':
         pass
     elif row[0] == '':
-        print '{0} {1}'.format(row[4],row[5]) 
+        print '{0} {1}'.format(scrub_callsign(row[1]),row[2]) 
     else:
-        print '{0} {1} {2} {3} {4} {5}'.format(row[0],row[1],row[2],row[3],row[4],row[5]) 
-    
+        #print '{0} {1} {2}'.format(row[0],row[1],row[2]) 
+        print '{0} {1} {2}'.format(scrub_timestamp(row[0]),scrub_callsign(row[1]),row[2])
+        
