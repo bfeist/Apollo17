c5f67fbe2e7df4353f222bfd500c8abd10a20610
Ben Feist <bf@benfeist.com>
2012-03-25 18:52:39 -0400

    updated processpipe.py to read all three csv parts

--- MC_Processing/processpipe.py
+++ MC_Processing/processpipe.py
@@ -40,47 +40,49 @@ def scrub_timestamp(timestamp):
 	return cleanTimestamp
     
 class BadNumberSub:
-    def __init__(self, number, badSubList):
-        self.number = number
-        self.badSubList = badSubList
+	def __init__(self, number, badSubList):
+		self.number = number
+		self.badSubList = badSubList
 
 def scrub_callsign(callsign):
-    if callsign == "MCC":
-        callsign = "CC"
-    return callsign
+	if callsign == "MCC":
+		callsign = "CC"
+	return callsign
 
-pageCounter = 499
+pageCounter = 8
 curRow = 0
 
-output_file_name_and_path = "F:\ApolloGit\Apollo17WIP\MC_Output\A17_TEC_501.1000.mc"
+output_file_name_and_path = "F:\ApolloGit\Apollo17WIP\MC_Output\A17_TEC.mc"
 outputFile = open(output_file_name_and_path, "w")
 
-reader = csv.reader(open("F:\ApolloGit\Apollo17WIP\OCR Output\A17_TEC_501-1000.csv", "rU"), delimiter='|')
-for row in reader:
-    curRow += 1
-    print curRow
-    outputLine = ''
-    #print(row)
-    if (row[0].startswith("Tape") or row[2].startswith("Tape")) and not row[2].startswith("Tape recorder"):
-    #if new page
-        if row[0].startswith("Tape"):
-            tapeNumber = row[0]
-        if row[2].startswith("Tape") :
-            tapeNumber = row[2]
-            
-        pageCounter += 1
-        outputLine = "\n\t{0}\n\tPage {1}\n".format(tapeNumber, pageCounter)
-        #print "\t{0}".format(tapeNumber, pageCounter)
-    elif row[0].startswith("Page") or row[2].startswith("Page"):
-        print "-----------------------" + str(pageCounter)
-    elif row[2] == '' or row[2] == 'APOLLO' or row[1] == 'APOLLO' or row[2] == 'END OF TAPE':
-        pass
-    elif row[0] == '':
-        outputLine = '{0} {1}\n'.format(scrub_callsign(row[1]),row[2]) 
-    else:
-        #print '{0} {1} {2}'.format(row[0],row[1],row[2]) 
-        outputLine = '{0} {1} {2}\n'.format(scrub_timestamp(row[0]),scrub_callsign(row[1]),row[2])
-        
-    outputFile.write(outputLine)
-    #print outputLine
+for curFile in [ "A17_TEC_1-500.csv", "A17_TEC_501-1000.csv", "A17_TEC_1001-2460.csv" ]:
+	inputFilePath = "F:\ApolloGit\Apollo17WIP\OCR Output\\" + curFile
+	reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
+	for row in reader:
+		curRow += 1
+		print curRow
+		outputLine = ''
+		#print(row)
+		if (row[0].startswith("Tape") or row[2].startswith("Tape")) and not row[2].startswith("Tape recorder"):
+			#if new page
+			if row[0].startswith("Tape"):
+				tapeNumber = row[0]
+			if row[2].startswith("Tape") :
+				tapeNumber = row[2]
+
+			pageCounter += 1
+			outputLine = "\n\t{0}\n\tPage {1}\n".format(tapeNumber, pageCounter)
+			#print "\t{0}".format(tapeNumber, pageCounter)
+		elif row[0].startswith("Page") or row[2].startswith("Page"):
+			print "-----------------------" + str(pageCounter)
+		elif row[2] == '' or row[2] == 'APOLLO' or row[1] == 'APOLLO' or row[2] == 'END OF TAPE':
+			pass
+		elif row[0] == '':
+			outputLine = '{0} {1}\n'.format(scrub_callsign(row[1]),row[2]) 
+		else:
+			#print '{0} {1} {2}'.format(row[0],row[1],row[2]) 
+			outputLine = '{0} {1} {2}\n'.format(scrub_timestamp(row[0]),scrub_callsign(row[1]),row[2])
+			
+		outputFile.write(outputLine)
+		#print outputLine
 outputFile.close()
