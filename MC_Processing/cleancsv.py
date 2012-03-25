2ecd15ad81d799979be4fb1fa67193531748dd55
Ben Feist <bf@benfeist.com>
2012-03-25 18:34:33 -0400

    Timestamps all cleaned. Many timestamps are still out of order.

--- MC_Processing/cleancsv.py
+++ MC_Processing/cleancsv.py
@@ -3,15 +3,17 @@ import sys
 import re
 
 def sterilize_token(token):
-	bs0 = BadNumberSub(0, ["o","Q","O"])
+	bs0 = BadNumberSub(0, ["o","Q","O","C","X"])
 	bs1 = BadNumberSub(1, ["i","J", "I","!","L","l"])
-	bs4 = BadNumberSub(4, ["h"])
+	bs4 = BadNumberSub(4, ["h", "^"])
 	bs6 = BadNumberSub(6, ["b"])
+	bs7 = BadNumberSub(7, ["?", "T"])
 	bs8 = BadNumberSub(8, ["B"])
+	bs9 = BadNumberSub(9, ["g"])
 	
 	tempToken = token
 	
-	for badSub in [ bs0, bs1, bs4, bs6, bs8 ]:
+	for badSub in [ bs0, bs1, bs4, bs6, bs7, bs8, bs9 ]:
 		for sub in badSub.badSubList:
 			tempToken = tempToken.replace(sub, str(badSub.number))
 
@@ -30,6 +32,11 @@ def scrub_timestamp(timestamp):
 	seconds = sterilize_token(values[i])
 	
 	cleanTimestamp = days + " " + hours + " " + minutes + " " + seconds
+	
+	testCleanTimestamp = cleanTimestamp.replace(' ', '')	
+	if not testCleanTimestamp.isdigit():
+		print "Uncleanable timestamp: " + cleanTimestamp + " - " + timestamp
+
 	return cleanTimestamp
 
 class BadNumberSub:
@@ -51,6 +58,7 @@ output_file_name_and_path = "F:\ApolloGit\Apollo17WIP\OCR Output\A17_TEC_cleaned
 outputFile = open(output_file_name_and_path, "w")
 
 callsignList = []
+lastTimestamp = 0
 
 for curFile in [ "A17_TEC_1-500.csv", "A17_TEC_501-1000.csv", "A17_TEC_1001-2460.csv" ]:
 	inputFilePath = "F:\ApolloGit\Apollo17WIP\OCR Output\\" + curFile
@@ -70,7 +78,7 @@ for curFile in [ "A17_TEC_1-500.csv", "A17_TEC_501-1000.csv", "A17_TEC_1001-2460
 				
 			pageCounter += 1
 			outputLine = "\n|||{0}|Page {1}\n".format(tapeNumber, pageCounter)
-			print "\t{0} - {1}".format(pageCounter, tapeNumber)
+			#print "\t{0} - {1}".format(pageCounter, tapeNumber)
 		elif row[0].startswith("Page") or row[2].startswith("Page"):
 			print "-----------------------" + str(pageCounter)
 		elif row[2] == '' or row[2] == 'APOLLO' or row[1] == 'APOLLO' or row[2] == 'END OF TAPE':
@@ -89,7 +97,11 @@ for curFile in [ "A17_TEC_1-500.csv", "A17_TEC_501-1000.csv", "A17_TEC_1001-2460
 			#	callsignList.append(scrubbedCallsign)
 			#	print "Page: " + str(pageCounter) + " : " + scrubbedCallsign
 			
-			#print '{0} {1} {2}'.format(row[0],row[1],row[2]) 
+			curTimestamp = scrub_timestamp(row[0]).replace(' ', '')	
+			if not curTimestamp > lastTimestamp:
+				print 'Timestamp out of order: Page{0} Timestamp:{1}'.format(pageCounter,row[0]) 
+			lastTimestamp = curTimestamp
+			
 			outputLine = '{0}|{1}|{2}\n'.format(scrub_timestamp(row[0]),scrubbedCallsign,row[2])
 			pass
 			
