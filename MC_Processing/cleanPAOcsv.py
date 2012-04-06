32cd2ee9fb547ca2068bfefbfa431f7905b76210
Ben Feist <bf@benfeist.com>
2012-04-06 19:09:10 -0400

    rebuilt PAO cleaner script

--- MC_Processing/cleanPAOcsv.py
+++ MC_Processing/cleanPAOcsv.py
@@ -1,6 +1,8 @@
 ﻿import csv
 import sys 
 import re
+import os
+
 
 def scrub_callsign(callsign):
 	#callsign = callsign.upper()
@@ -9,46 +11,69 @@ def scrub_callsign(callsign):
 		callsign = "CC"
 	return callsign
 
-readPageCounter = 8
+readPageCounter = 1
 curRow = 0
 errorCount = 0
+newTimestamp = 0
 
 output_file_name_and_path = "F:\ApolloGit\Apollo17WIP\OCR Output\A17_PAO_cleaned.csv"
-#outputFile = open(output_file_name_and_path, "w")
+outputFile = open(output_file_name_and_path, "w")
+outputLine = ""
+pageTitle = ""
 
 callsignList = [ "LAUNCH CNTL", "CAPCOM", "PAO", "SC", "AMERICA", "CERNAN", "SCHMITT", "CHALLENGER", "RECOVERY", "SPEAKER", "GREEN", "ECKER", "BUTTS", "JONES", "EVANS", "HONEYSUCKLE"  ]
-lastTimestamp = 0
+
 
 for curFile in [ "A17_PAO.csv" ]:
 	inputFilePath = "F:\ApolloGit\Apollo17WIP\OCR Output\\" + curFile
 	reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
 	for row in reader:
 		curRow += 1
-		outputLine = ''
 		#print row[1]
 		if len(row) > 1:
 			if row[1].startswith("APOLLO 17 MISSION COMMENTARY") :
-				#print str(curRow) + "-----------------------" + row[1]
+				#pageTitle = "||" + row[1] + "|Page " + str(readPageCounter)
+				pageTitle = "||" + row[1]
 				readPageCounter += 1
+				
+				cstTimeStr = row[1][row[1].index("CST") + 4:row[1].index("CST") + 10]
+				getTimeStr = row[1][row[1].index("GET") + 4:row[1].index("GET") + 10]
+				if getTimeStr.find("/") != -1 or getTimeStr.find("MC") != -1 or cstTimeStr.find("/") != -1 or cstTimeStr.find("MC") != -1:
+					#time must be to the left of 'GET'
+					getTimeStr = row[1][row[1].index("GET") -8 :row[1].index("GET")]
+				getTimeStr = max(getTimeStr.strip().split(" "), key=len).replace(",","").zfill(6) #strip longest list item and remove commas - this is the timestamp
+				print str(curRow) + " " + getTimeStr
+				#outputFile.write("TIMESTAMP|" + row[1] + "\n")
+				newTimestamp = 1
+				
 			elif row[1].startswith("END OF TAPE"):
-				pass
+				pass #delete END OF TAPE rows
 			else:
 				if any(row[0].startswith(curCallsign) for curCallsign in callsignList):
 					#print "-----------------------" + "Callsign Found.\n"
-					pass
+					outputFile.write("\n")
+					if newTimestamp == 1 :
+						outputLine = pageTitle + "\n" #print the complete typed pages title
+						outputLine += '{0}|{1}|{2}'.format(getTimeStr,row[0],row[1]) #print the first dialogue with the timestamp from the title
+						newTimestamp = 0
+					else :
+						outputLine = '|{0}|{1}'.format(row[0],row[1]) #print a regular line of dialog with callsign
 				else:
-					print str(curRow) + "-----------------------" + "Callsign Not Found. " + str(len(row)) + " " + str(row[0])
+					print str(curRow) + "-----------------------" + "Callsign Not Found. " + str(len(row)) + " " + str(row)
 					errorCount += 1
 				#print row
-				#outputLine = '{0}|{1}|{2}\n'.format(scrub_timestamp(row[0]),scrubbedCallsign,row[2])
+				#print outputLine
 				pass
 		else:
-			#print str(curRow) + " has only one element: " + row[0]
+			#--only one record, so it must be a continuation of the previous line
+			outputLine += ' ' + row[0]
+			#print str(curRow) + " concatted line: " + outputLine
 			pass
 		
-		if errorCount > 200:
-			break
+		#if readPageCounter % 60 == 0:
+		#	break
 			
-		#outputFile.write(outputLine)
+		outputFile.write(outputLine)
+		outputLine = ""
 		#print outputLine
 #outputFile.close()
