9144be4fb539e47f5731f47991aa88eb9a687f86
Ben Feist <bf@benfeist.com>
2012-04-07 21:23:21 -0400

    updated to output PAO-only as well as full transcript files

--- Processing_Scripts/cleanPAOcsv.py
+++ Processing_Scripts/cleanPAOcsv.py
@@ -17,16 +17,18 @@ errorCount = 0
 newTimestamp = 0
 lastWasPAO = 0
 
-output_file_name_and_path = "F:\ApolloGit\Apollo17WIP\OCR Output\A17_PAO_cleaned.csv"
+output_file_name_and_path = "F:\ApolloGit\Apollo17\Processing_Output\A17_PAO_cleaned.csv"
+output_file_name_and_path_PAO_only = "F:\ApolloGit\Apollo17\Processing_Output\A17_PAO_cleaned_pao_only.csv"
 outputFile = open(output_file_name_and_path, "w")
+PAOOnlyOutputFile = open(output_file_name_and_path_PAO_only, "w")
+PAOOnlyOutputLine = ""
 outputLine = ""
 pageTitle = ""
 
 callsignList = [ "LAUNCH CNTL", "CAPCOM", "PAO", "SC", "AMERICA", "CERNAN", "SCHMITT", "CHALLENGER", "RECOVERY", "SPEAKER", "GREEN", "ECKER", "BUTTS", "JONES", "EVANS", "HONEYSUCKLE"  ]
 
-
 for curFile in [ "A17_PAO.csv" ]:
-	inputFilePath = "F:\ApolloGit\Apollo17WIP\OCR Output\\" + curFile
+	inputFilePath = "F:\ApolloGit\Apollo17\OCR_Output\\" + curFile
 	reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
 	for row in reader:
 		curRow += 1
@@ -43,7 +45,7 @@ for curFile in [ "A17_PAO.csv" ]:
 				if getTimeStr.find("/") != -1 or getTimeStr.find("MC") != -1 or cstTimeStr.find("/") != -1 or cstTimeStr.find("MC") != -1:
 					#time must be to the left of 'GET'
 					getTimeStr = row[1][row[1].index("GET") -8 :row[1].index("GET")]
-				getTimeStr = max(getTimeStr.strip().split(" "), key=len).replace(",","").zfill(6) #strip longest list item and remove commas - this is the timestamp
+				getTimeStr = max(getTimeStr.strip().split(" "), key=len).replace(",","").zfill(6) + ":00" #strip longest list item and remove commas - this is the timestamp
 				print str(curRow) + " " + getTimeStr
 				#outputFile.write("TIMESTAMP|" + row[1] + "\n")
 				newTimestamp = 1
@@ -53,8 +55,7 @@ for curFile in [ "A17_PAO.csv" ]:
 			else:
 				if any(row[0].startswith(curCallsign) for curCallsign in callsignList):
 					#print "-----------------------" + "Callsign Found.\n"
-					#if row[0].startswith("PAO"):
-					#lastWasPAO = 1
+
 					outputFile.write("\n")
 					if newTimestamp == 1 :
 						outputLine = pageTitle + "\n" #print the complete typed pages title
@@ -62,8 +63,13 @@ for curFile in [ "A17_PAO.csv" ]:
 						newTimestamp = 0
 					else :
 						outputLine = '|{0}|{1}'.format(row[0],row[1]) #print a regular line of dialog with callsign
-					#else:
-					#	lastWasPAO = 0
+						
+					if row[0].startswith("PAO"):
+						PAOOnlyOutputFile.write("\n")
+						lastWasPAO = 1
+						PAOOnlyOutputLine = outputLine						
+					else:
+						lastWasPAO = 0
 						
 				else:
 					print str(curRow) + "-----------------------" + "Callsign Not Found. " + str(len(row)) + " " + str(row)
@@ -73,9 +79,12 @@ for curFile in [ "A17_PAO.csv" ]:
 				pass
 		else:
 			#--only one record, so it must be a continuation of the previous line
-			#if lastWasPAO == 1:
 			outputLine += ' ' + row[0]
-			#lastWasPAO = 0
+			
+			if lastWasPAO == 1:
+				PAOOnlyOutputLine += ' ' + row[0]
+				lastWasPAO = 0
+			
 			#print str(curRow) + " concatted line: " + outputLine
 		
 		#if readPageCounter % 60 == 0:
@@ -83,5 +92,8 @@ for curFile in [ "A17_PAO.csv" ]:
 			
 		outputFile.write(outputLine)
 		outputLine = ""
+		
+		PAOOnlyOutputFile.write(PAOOnlyOutputLine)
+		PAOOnlyOutputLine = ""
 		#print outputLine
 #outputFile.close()
