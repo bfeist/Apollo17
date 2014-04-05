56481b9c02167645cd8c9efce7a8cfb743e789e5
bfeist <bf@benfeist.com>
2014-04-05 16:54:24 -0400

    Created error correction script. Created index file to track sleep periods, missing audio, etc. Fixed timing errors up to line 15000

---
+++ Processing_Scripts/errorcheckCleanedTranscript.py
@@ -0,0 +1,24 @@
+﻿import csv
+import sys 
+import re
+
+curRow = 0
+prevTimestamp = 0
+for curFile in [ "A17 master TEC and PAO manual merge.csv" ]:
+	inputFilePath = "E:\Apollo17WIP\Processing_Output\\" + curFile
+	reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
+	for row in reader:
+		curRow += 1
+		if row[1].__len__() > 9:
+			print curRow
+			print "------------- timestamp too long: " + row[1]
+		intTimestamp = row[1].translate(None, ":") #remove colons
+		if intTimestamp != "":
+			if int(intTimestamp) < prevTimestamp:
+				print "Timestamp order error on line " + str(curRow)
+			prevTimestamp = int(intTimestamp)
+		#print(intTimestamp)
+		#print outputLine
+		if curRow > 15000:
+			break
+print "DONE"
