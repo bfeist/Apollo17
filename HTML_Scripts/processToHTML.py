a5c12cb00cf5aadb811c4122be6fcedb2258089b
Ben Feist <ben.feist@taxi.ca>
2014-04-22 13:10:14 -0400

    python template attempt

---
+++ HTML_Scripts/processToHTML.py
@@ -0,0 +1,16 @@
+#from jinja2 import Template
+#template = Template('Hello {{ name }}!')
+#print template.render(name='John Doe')
+
+import csv
+
+from jinja2 import Environment, PackageLoader
+env = Environment(loader=PackageLoader('processToHTML', './templates'))
+template = env.get_template('testtemplate.html')
+print template.render(var1='test1', var2='test2')
+
+inputFilePath = "../Processing_Output/A17 master TEC and PAO manual merge.csv"
+reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
+csv_data = [ row for row in reader ]
+
+print template.render( data=csv_data )
