f7f74d25d900296092480698f2c7568510fd31a6
bfeist <bf@benfeist.com>
2015-02-15 13:59:19 -0500

    Created HTML TOC script

--- HTML_Scripts/processToHTML.py
+++ HTML_Scripts/processToHTML.py
@@ -17,19 +17,17 @@ outputFile.write(template.render({'datarow': 0},
 
 #WRITE BODY ITEMS
 curRow = 0
-for curFile in [ "A17 master TEC and PAO manual merge.csv" ]:
-#for curFile in ["temp.csv"]:
-    inputFilePath = "E:\Apollo17WIP\Processing_Output\\" + curFile
-    reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
-    for row in reader:
-        curRow += 1
-        if row[1] != "": #if not a TAPE change or title row
-            loader = FileLoader('templates')
-            template = loader.load_template('template_timelineitem.html')
-            outputFile.write(template.render({'timestamp': row[1], 'who': row[2], 'words': row[3]},
-                                 loader=loader).encode('utf-8'))
-        #if curRow > 40000:
-        #    break
+inputFilePath = "E:\Apollo17.org\MISSION_DATA\A17 master TEC and PAO utterances.csv"
+reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
+for row in reader:
+    curRow += 1
+    if row[1] != "": #if not a TAPE change or title row
+        loader = FileLoader('templates')
+        template = loader.load_template('template_timelineitem.html')
+        outputFile.write(template.render({'timestamp': row[1], 'who': row[2], 'words': row[3]},
+                             loader=loader).encode('utf-8'))
+    if curRow > 1000:
+        break
 
 #WRITE FOOTER
 template = loader.load_template('template_footer.html')
