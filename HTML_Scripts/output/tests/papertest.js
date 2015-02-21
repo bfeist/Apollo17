ccefbae20d98e910ce20877cd44347123be6ccf3
bfeist <bf@benfeist.com>
2015-02-21 17:40:58 -0500

    Transitioning to single HTML file. Mission TOC.csv in progress. Fixed bad video mask for 234:40 video

--- HTML_Scripts/output/tests/papertest.js
+++ HTML_Scripts/output/tests/papertest.js
@@ -1,20 +1,20 @@
 // Create a circle shaped path with its center at the center
 // of the view and a radius of 30:
 
-var text = new PointText({
-    point: view.center,
+var text = new paper.PointText({
+    point: paper.view.center,
     justification: 'center',
     fontSize: 30,
     fillColor: 'grey',
     content: 'text'
 });
 
-var mouseFollowPath = new Path();
+var mouseFollowPath = new paper.Path();
 mouseFollowPath.strokeColor = 'black';
 
 function onResize(event) {
     // Whenever the window is resized, recenter the path:
-    text.point = view.center;
+    text.point = paper.view.center;
 }
 
 function onMouseMove(event) {
@@ -22,4 +22,78 @@ function onMouseMove(event) {
     mouseFollowPath.add(event.point.x, 0);
     mouseFollowPath.add(event.point.x, view.size.height);
     text.content = event.point.x;
+}
+
+//First define the zoom tool properties
+
+var toolZoomIn = new paper.Tool();
+
+toolZoomIn.distanceThreshold = 8;
+toolZoomIn.mouseStartPos = new paper.Point();
+toolZoomIn.zoomFactor = 2;
+
+//Let's draw a reference rectangle to compare between grids.
+
+var rectangle = new paper.Rectangle(new paper.Point(50, 50), new paper.Point(150, 100));
+var path = new paper.Path.Rectangle(rectangle);
+path.fillColor = '#e9e9ff';
+path.selected = false;
+
+var gridGroup = new paper.Group;
+drawGrid();
+
+//The function that draws horizontal/vertical lines
+
+function drawGridOnScreen(){
+
+//Width/Height per cell on the grid variables
+
+    var widthPerCell = 50;
+    var heightPerCell = 50;
+
+//Draw the grid lines and add them into the global group above that holds all the lines
+
+    var drawGridLines = function(num_rectangles_wide, num_rectangles_tall, boundingRect) {
+
+        for (var i = 0; i <= num_rectangles_wide; i++) {
+            var correctedBoundingRectLeft = Math.ceil(boundingRect.left/widthPerCell) * widthPerCell;
+            var xPos = correctedBoundingRectLeft + i * widthPerCell;
+            var topPoint = new paper.Point(xPos, boundingRect.top);
+            var bottomPoint = new paper.Point(xPos, boundingRect.bottom);
+            var aLine = new paper.Path.Line(topPoint, bottomPoint);
+            aLine.strokeColor = '968d8d';
+            gridGroup.addChild(aLine);
+        }
+
+        for (i = 0; i <= num_rectangles_tall; i++) {
+            var correctedBoundingRectTop = Math.ceil(boundingRect.top/heightPerCell) * heightPerCell;
+            var yPos = correctedBoundingRectTop + i * heightPerCell;
+            var leftPoint = new paper.Point(boundingRect.left, yPos);
+            var rightPoint = new paper.Point(boundingRect.right, yPos);
+            var bLine = new paper.Path.Line(leftPoint, rightPoint);
+            bLine.strokeColor = '968d8d';
+            gridGroup.addChild(bLine);
+        }
+    }
+//Find out how many cells we need vertically/horizontally first and then call the ''grid maker'' function to draw them.
+
+    var numberOfVerticalCells = paper.view.bounds.width / widthPerCell;
+    var numberOfHorizontalCells = paper.view.bounds.height / heightPerCell;
+
+    drawGridLines(numberOfVerticalCells, numberOfHorizontalCells, paper.view.bounds);
+}
+
+//This function gets called on each mouse zoom interval. If there is a previously drawn grid, delete it and start over.
+
+function drawGrid(){
+
+    if (gridGroup.isEmpty()) {
+        drawGridOnScreen();
+    }
+
+    else {
+        gridGroup.removeChildren();
+        drawGridOnScreen();
+    }
+
 }
--- HTML_Scripts/processToHTML.py
+++ HTML_Scripts/processToHTML.py
@@ -9,25 +9,24 @@ outputFile.close()
 
 outputFile = open(output_file_name_and_path, "a")
 
-loader = FileLoader('templates')
-#WRITE HEADER
-template = loader.load_template('template_header.html')
-outputFile.write(template.render({'datarow': 0},
-            loader=loader).encode('utf-8'))
+template_loader = FileLoader('templates')
 
-#WRITE BODY ITEMS
-curRow = 0
-inputFilePath = "E:\Apollo17.org\MISSION_DATA\A17 master TEC and PAO utterances.csv"
-reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
-for row in reader:
-    curRow += 1
-    if row[1] != "": #if not a TAPE change or title row
-        loader = FileLoader('templates')
-        template = loader.load_template('template_timelineitem.html')
-        outputFile.write(template.render({'timestamp': row[1], 'who': row[2], 'words': row[3]}, loader=loader).encode('utf-8'))
-    #if curRow > 1000:
-    #    break
+#WRITE HEADER
+item_template = template_loader.load_template('template_header.html')
+outputFile.write(item_template.render({'datarow': 0}, loader=template_loader).encode('utf-8'))
+
+#WRITE SEGMENT BODY ITEMS
+cur_row = 0
+input_file_path = "E:\Apollo17.org\MISSION_DATA\A17 master TEC and PAO utterances.csv"
+utterance_reader = csv.reader(open(input_file_path, "rU"), delimiter='|')
+for utterance_row in utterance_reader:
+    cur_row += 1
+    timeid = "timeid" + utterance_row[1].translate(None, ":")
+    if utterance_row[1] != "": #if not a TAPE change or title row
+        item_template = template_loader.load_template('template_timelineitem.html')
+        outputFile.write(item_template.render({'timeid': timeid, 'timestamp': utterance_row[1], 'who': utterance_row[2], 'words': utterance_row[3]}, loader=template_loader).encode('utf-8'))
+
 
 #WRITE FOOTER
-template = loader.load_template('template_footer.html')
-outputFile.write(template.render({'datarow': 0}, loader=loader).encode('utf-8'))
+item_template = template_loader.load_template('template_footer.html')
+outputFile.write(item_template.render({'datarow': 0}, loader=template_loader).encode('utf-8'))
