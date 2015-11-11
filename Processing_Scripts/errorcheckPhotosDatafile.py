ea0821cec88d1b8707c0e0083d65fe68c8a0b9e7
Ben Feist <ben.feist@taxi.ca>
2015-11-11 14:46:14 -0500

    Photo error checking script and partial photo data corrections in xls

---
+++ Processing_Scripts/errorcheckPhotosDatafile.py
@@ -0,0 +1,24 @@
+﻿import csv
+
+curRow = 0
+
+listTimestamps = []
+
+for curFile in ["photos.csv"]:
+	inputFilePath = "../MISSION_DATA/" + curFile
+	reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
+	for row in reader:
+		curRow += 1
+		if row[0].__len__() > 9:
+			print curRow
+			print "------------- timestamp too long: " + row[0]
+
+		timestamp = row[0]
+
+		if timestamp != "" and timestamp != "skip":
+			if timestamp in listTimestamps:
+				print "timestamp already used: " + row[0]
+			else:
+				listTimestamps.append(timestamp)
+
+print "DONE"
